fec6cee2fb9a3f75d8a799c84c8f2252eefed498
Melissa Ferguson <melissafergusondesign@gmail.com>
2020-12-08 10:48:15 -0500

    User Profile Updates

---
+++ final-capstone/front-end/vue/src/components/ProfilePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="user-profile"> <!-- One div to rule them all -->
+        <div class="pet-profile-bar"> <!-- This is where the pet profile section goes-->
+        <img src = "https://res.cloudinary.com/ashdav/image/upload/v1607440049/img/cxk6sloi51lhdwindzxi.png">
+        </div>
+
+    </div>
+    <!-- -->
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
