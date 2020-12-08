8690a0aafc3b8b3c1af161f92bcdaccf89b72338
Ashley Davis <AshleyMDavis109@gmail.com>
2020-12-08 09:02:47 -0500

    image dropbox code added to Homepage.vue

--- final-capstone/front-end/vue/src/components/Homepage.vue
+++ final-capstone/front-end/vue/src/components/Homepage.vue
@@ -1,5 +1,18 @@
 <template>
     <div class="home">
+        <!-- eslint-disable --> <!-- This disables annoying eslink warning messages in the html       -->
+        <!-- This is the dropzone component that will give a place to drop the image to be uploaded -->
+        <!-- there are two custom events the component listens for:                                 -->
+        <!--       the vdropzone-sending event which is fired when dropzone is sending an image     -->
+        <!--       the vdropzone-success event which is fired when dropzone upload is successful    -->
+        <vue-dropzone
+            id="dropzone"
+            class="mt-3"
+            v-bind:options="dropzoneOptions"
+            v-on:vdropzone-sending="addFormData"
+            v-on:vdropzone-success="getSuccess"
+            :useCustomDropzoneOptions="true"
+        ></vue-dropzone>
         <div class="main">
             <header>
                 <img src="https://cdn.freebiesupply.com/logos/large/2x/tinder-1-logo-black-and-white.png" />
@@ -11,7 +24,7 @@
             </section>
         </div>
         <body>
-            <!--Homepage graphic goes here-->
+            <img id="home-graphic" src="https://res.cloudinary.com/ashdav/image/upload/v1607434478/img/b9wguibeb6nzbug8zrob.png"/>
         </body>
         <footer> 
             <p>© 2020 Pet Play Pals. All Rights Reserved</p>
@@ -20,10 +33,59 @@
 </template>
 
 <script>
+/* eslint-disable */
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
+
 export default {
-    
+    name: "upload-photo",
+    components: {
+        vueDropzone: vue2Dropzone
+    },
+    data(){
+        return {
+            //-------------------------------------------------------------------------------------
+            // TODO: substitute your actual Cloudinary cloud-name where indicated in the URL
+            //-------------------------------------------------------------------------------------
+            dropzoneOptions: {
+                url: "https://api.cloudinary.com/v1_1/ashdav/image/upload",  
+                thumbnailWidth: 250,
+                thumbnailHeight: 250,
+                maxFilesize: 2.0,
+                acceptedFiles: ".jpg, .jpeg, .png, .gif",
+                uploadMultiple: false,
+                addRemoveLinks: true,
+                dictDefaultMessage: 'Drop files here to upload. </br> Alternatively, click to select a file for upload.',                
+            },      
+        }
+    },
+
+    methods:{
+        /******************************************************************************************
+         * The addFormData method is called when vdropzone-sending event is fired
+         * it adds additional headers to the request
+         ******************************************************************************************/
+        //--------------------------------------------------------------------------------------------
+        // TODO: substitute your actual Cloudinary api-key where indicated in the following code
+        // TODO: substitute your actual Cloudinary upload preset where indicated in the following code
+        //----------------------------------------------------------------------------==---------------   
+        addFormData(file, xhr, formData) {
+            formData.append("api_key", "657265325953443");                // substitute your api key
+            formData.append("upload_preset", "pfjwkuav");   // substitute your upload preset
+            formData.append("timestamp", (Date.now() / 1000) | 0);
+            formData.append("tags", "vue-app");
+        },
+         /******************************************************************************************
+         * The getSuccess method is called when vdropzone-success event is fired
+         ******************************************************************************************/
+        getSuccess(file, response) {
+            const imgUrl = response.secure_url;   // store the url for the uploaded image
+            this.$emit("image-upload", imgUrl);   // fire custom event with image url in case someone cares
+        },
+    }
 }
 </script>
+
 
 <style scoped>
 
@@ -33,7 +95,7 @@ export default {
     flex-direction: column;
 }
 .main {
-    background-image: url(https://besthqwallpapers.com/Temp/8-12-2020/cavalier-king-charles-spaniel-black-cat-pets-dogs-friendship-besthqwallpapers.com-1920x1080.jpg);
+    background-image: url(https://res.cloudinary.com/ashdav/image/upload/v1607434473/img/lmvvi7mkx5f2fmbdh2xi.jpg);
     background-size: cover;
     display: grid;
     grid-template-rows: 1fr 1fr 1fr;
@@ -82,6 +144,19 @@ button {
     color: white;
     font-size: 20px;
     padding: 5px 15px;
+}
+body {
+    display: grid;
+    grid-template: 1fr 1fr 1fr; 
+    grid-template-areas: 
+        "."
+        "graphic"
+        ".";
+}
+#home-graphic {
+    grid-area: graphic;
+    width: 1200px;
+    height: 300px;
 }
 footer {
     background-color: #1aebeb;
