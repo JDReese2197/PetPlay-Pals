32fa54c68858a575c916d057d192f5dab1c90eb2
Melissa Ferguson <melissafergusondesign@gmail.com>
2020-12-11 14:16:40 -0500

    PlayDate updates

--- final-capstone/front-end/vue/src/components/PlayDateCard.vue
+++ final-capstone/front-end/vue/src/components/PlayDateCard.vue
@@ -1,33 +1,25 @@
 <template>
     <div class="playdate-card"> <!-- A single playdate card-->
         <!-- Need to display: pet_poster, date, start-end time, location, details, pet img-->
-        <img id = "pet-pic" src = "https://res.cloudinary.com/ashdav/image/upload/v1607530571/img/Rudy_dt7y3q.png"/>
+        <!-- <img id ="pet-pic" src ="https://res.cloudinary.com/ashdav/image/upload/v1607530571/img/Rudy_dt7y3q.png"/>
         <h1> PET NAME </h1>
         <h4> date </h4>
         <h4> time </h4>
         <h4> location </h4>
-        <p>Details</p>
+        <p>Details</p> -->
         <!-- easy option to accept or decline playdate-->
         <!-- maybe spot to send messages????-->
     </div>
 </template>
 
 <script>
+import PetCard from './PetCard.vue'
 export default {
+  components: { PetCard },
     name: "play-date-card",
     data() {
         return {
-            playdate: {
-                petImage: '',
-                petName: '',
-                petGender: '',
-                petType: '',
-                petBreed: '',
-                petSize: '',
-                petAge: '',
-                petPersonality: '',
-                petDescription: '',
-            }    
+            
         }
     }
 }
